94e3b65106edc0b2c209a00b58ecde22e8a5ac25
alif <malifalhakim11@gmail.com>
2023-11-22 09:37:03 +0700

    Ubah solusi untuk CSRF verification failed

--- book_nest/settings.py
+++ book_nest/settings.py
@@ -145,3 +145,5 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
+
+CSRF_TRUSTED_ORIGINS = ['https://malif-al-tugas.pbp.cs.ui.ac.id']
